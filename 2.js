46983e2dc048a03e2a2cb5c02953d92a516637cf
isaac <isaacyslavin@gmail.com>
2020-09-17 19:13:35 -0700

    d

--- 2.js
+++ 2.js
@@ -2,8 +2,10 @@ var tend=require("http")
 var our=require("fs")
 tend.createServer(function(coby,isaac){
 isaac.setHeader("content-type","text/html")
+if(coby.url=="/")
 isaac.write(our.readFileSync("new 2.html"))
-
+if(coby.url=="/user")
+isaac.write("ren")
 isaac.end("url")
 
 
